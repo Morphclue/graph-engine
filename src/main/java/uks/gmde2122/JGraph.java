298442d15d172d84d241383001c31424fe020626
Julian Holfeld <julian@uni-kassel.de>
2021-11-17 11:09:15 +0100

    test: add base scenario as nodes

--- src/main/java/uks/gmde2122/JGraph.java
+++ src/main/java/uks/gmde2122/JGraph.java
@@ -2,4 +2,7 @@ package uks.gmde2122;
 
 public class JGraph
 {
+    public JNode createNode() {
+        return null;
+    }
 }
--- src/test/java/uks/gmde2122/FerrymanTest.java
+++ src/test/java/uks/gmde2122/FerrymanTest.java
@@ -4,9 +4,14 @@ import org.junit.Test;
 
 public class FerrymanTest {
     @Test
-    public void testFerrymanProblem(){
+    public void testFerrymanProblem() {
         JGraph startGraph = new JGraph();
 
-        JNode wolf = new JNode().putAttribute("label", "wolf");
+        JNode wolf = startGraph.createNode().putAttribute("label", "wolf");
+        JNode goat = new JNode().putAttribute("label", "goat");
+        JNode cabbage = new JNode().putAttribute("label", "cabbage");
+        JNode boat = new JNode().putAttribute("label", "boat");
+        JNode leftBank = new JNode().putAttribute("label", "bank").putAttribute("side", "left");
+        JNode rightBank = new JNode().putAttribute("label", "bank").putAttribute("side", "right");
     }
 }
